4f8fc48f07e4b56f62fc0ba5bd081d3930f539d6
Dim Ruzavin <ruzavin1612@gmail.com>
2016-11-11 15:36:51 +0300

    task2b

--- src/index.js
+++ src/index.js
@@ -14,6 +14,23 @@ app.get('/task2A', (req, res) => {
   res.send(sum.toString());
 });
 
+app.get('/task2B', (req, res) => {
+  let fullName = req.query.fullname || '';
+  let arr = fullName.split(' ');
+  let len = arr.length;
+
+  if (fullName === '' || len > 3 || /\d|_|\//.test(fullName)) {
+    fullName = 'Invalid fullname';
+  } else {
+    fullName = arr[len - 1];
+    for (let i = 0; i < len - 1; i++) {
+      fullName += ' ' + arr[i].slice(0, 1) + '.';
+    }
+  }
+
+  res.send(fullName);
+});
+
 app.listen(3000, () => {
   console.log('Your app listening on port 3000!');
 });
