a24ffc7cea103151810fd9094cfd3c1ac127b92d
Dim Ruzavin <ruzavin1612@gmail.com>
2016-11-19 20:34:35 +0300

    2c

--- src/index.js
+++ src/index.js
@@ -1,36 +1,19 @@
-import express from 'express';
-import cors from 'cors';
+var express = require('express');
+var cors = require('cors');
 
 const app = express();
+
 app.use(cors());
-app.get('/', (req, res) => {
-  res.json({
-    hello: 'JS World',
-  });
+app.get('/task2C', (req, res, next) => {
+
+  const username = typeof (req.query.username) == 'undefined' ? 'Empty Empty Empty'
+   : req.query.username;
+  const result = /[a-z\d_\-.]+$/gi.exec(username.replace(/\?.+$/gi, '')
+.replace(/(https?:\/\/[^\/]+\/[^\/]+)\/.+$/gi, '$1'));
+  console.log(`Query: ${req.originalUrl} <br>-<br>Вывод: @${result[0]}`);
+  res.send('@' + result[0]);
+
 });
 
-app.get('/task2A', (req, res) => {
-  const sum = (+req.query.a || 0) + (+req.query.b || 0);
-  res.send(sum.toString());
-});
-
-app.get('/task2B', (req, res) => {
-  let fullName = req.query.fullname || '';
-  let arr = fullName.split(' ');
-  let len = arr.length;
-
-  if (fullName === '' || len > 3 || /\d|_|\//.test(fullName)) {
-    fullName = 'Invalid fullname';
-  } else {
-    fullName = arr[len - 1];
-    for (let i = 0; i < len - 1; i++) {
-      fullName += ' ' + arr[i].slice(0, 1) + '.';
-    }
-  }
-
-  res.send(fullName);
-});
-
-app.listen(3000, () => {
-  console.log('Your app listening on port 3000!');
+app.listen(3000, () => {console.log('App listening on port 3000!');
 });
